5d4a684b5da73b76fa96118d7d43e78e7bd7ea03
Luis C. Vargas <luiscvargasb@gmail.com>
2021-12-07 23:46:00 -0500

    add placeholder file for nav bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './m51.jpg';
 import './App.css';
+import React, { Component } from 'react';
+import TopNavBar from './topNavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is a WIP professional page. Stay tuned!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              This is a WIP professional page. Stay tuned!
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+            Learn React
+            </a>
+        </header>
+        <TopNavBar />
     </div>
   );
 }
---
+++ src/topNavBar.js
@@ -0,0 +1,14 @@
+import React, { Container } from 'react'; 
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+
+class TopNavBar extends React.Component {
+    render() { 
+        //return (
+        //    <Navbar bg="light" expand="lg">
+        //    </Navbar>
+        //)
+        return <h1>test</h1>
+    }
+}
+
+export default TopNavBar;
